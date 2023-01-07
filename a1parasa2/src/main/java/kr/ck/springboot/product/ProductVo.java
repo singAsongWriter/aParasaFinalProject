6b023c8df5c32403a92039b348142786ad27abfd
joonsen <safety9876@naver.com>
2023-01-07 16:35:59 +0900

    merge

---
+++ a1parasa2/src/main/java/kr/ck/springboot/product/ProductVo.java
@@ -0,0 +1,14 @@
+package kr.ck.springboot.product;
+
+public class ProductVo {
+	String id;
+	String supplierId;
+	String name;
+	String contents;
+	int price;
+	int stock;
+	String categoryId;
+	String createdAt;
+	String updatedAt;
+	int deliveryFee;
+}
