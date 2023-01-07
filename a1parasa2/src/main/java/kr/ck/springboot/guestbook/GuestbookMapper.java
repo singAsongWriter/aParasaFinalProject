5c21a64cc7eb22a96eeb2a1169181ff6cd8977aa
Coleft <108502493+coleft@users.noreply.github.com>
2023-01-07 13:27:15 +0900

    필요없는 파일들을 정리

--- a1parasa2/src/main/java/kr/ck/springboot/guestbook/GuestbookMapper.java
+++
@@ -1,7 +0,0 @@
-package kr.ck.springboot.guestbook;
-
-import java.util.List;
-
-public interface GuestbookMapper {
-	public List<GuestbookVo> list(String findStr);
-}
--- a1parasa2/src/main/java/kr/ck/springboot/guestbook/GuestbookService.java
+++
@@ -1,5 +0,0 @@
-package kr.ck.springboot.guestbook;
-
-public class GuestbookService {
-
-}
